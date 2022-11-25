c0be47c6eab67564cc4f3d63345346b2a7f65ceb
helena <heju1958@outlook.com>
2022-11-25 12:35:41 -0300

    feat: estructure jest

---
+++ src/__test__/footer.test.tsx
@@ -0,0 +1,16 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { describe, expect, test } from "@jest/globals";
+import { screen, render } from "@testing-library/react";
+import Footer from "../components/Footer/footer";
+
+describe("Footer Render", () => {
+  test("Show footer in screen", () => {
+    render(<Footer />);
+    expect(
+      screen.getByText("MKS sistemas © Todos os direitos reservados")
+    ).toBeTruthy();
+  });
+});
